071567b9b60319bfbe41164feda2c4838cf44d1c
Matt Kubej <matt.kubej@shopify.com>
2021-09-07 12:02:02 -0400

    switching to 0.5, nightly got removed

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ install_stow() {
 install_nvim_nightly() {
   echo "\n  -- installing nvim nightly -- \n"
 
-  curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
+  curl -L https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage \
     -o /tmp/nvim.appimage
 
   chmod u+x /tmp/nvim.appimage
