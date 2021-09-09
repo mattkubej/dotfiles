353b7f76b4250b3c0a8d3f88424c2bf179ef3e6e
Matt Kubej <matt.kubej@shopify.com>
2021-09-09 18:13:27 -0400

    add formatter for typescriptreact

--- nvim/.config/nvim/lua/mk/plugins/configs/formatter.lua
+++ nvim/.config/nvim/lua/mk/plugins/configs/formatter.lua
@@ -1,16 +1,16 @@
+local prettier = function()
+  return {
+    exe = "prettier",
+    args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+    stdin = true
+  }
+end
+
 require('formatter').setup({
   logging = false,
   filetype = {
-    javascript = {
-        -- prettier
-       function()
-          return {
-            exe = "prettier",
-            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
-            stdin = true
-          }
-        end
-    },
+    javascript = { prettier },
+    typescriptreact = { prettier },
   }
 })
 
