ce9506ee06b9091041aaeb1fb9140ed95e92200b
Matt Kubej <matt.kubej@shopify.com>
2021-09-24 17:20:13 -0400

    switch to 0.5

--- install.sh
+++ install.sh
@@ -85,7 +85,11 @@ install_prettier() {
 install_nvim_nightly() {
   echo "\n  -- installing nvim nightly -- \n"
 
-  curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
+  # nightly disappeared?!
+  #curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
+    #-o /tmp/nvim.appimage
+
+  curl -L https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage \
     -o /tmp/nvim.appimage
 
   chmod u+x /tmp/nvim.appimage
