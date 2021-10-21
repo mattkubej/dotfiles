3b62ffc94a3111b6656d8267a474098c0b133337
Matt Kubej <matt.kubej@shopify.com>
2021-10-21 18:50:24 -0400

    single line

--- install.sh
+++ install.sh
@@ -144,8 +144,7 @@ bootstrap_nvim_packer() {
     -c 'autocmd User PackerComplete quitall' -c 'PackerSync' &> /dev/null
 
   echo "\n -- updating treesitter -- \n"
-  nvim --headless \
-    -c 'TSUpdateSync' &> /dev/null
+  nvim --headless -c 'TSUpdateSync' &> /dev/null
 }
 
 if [ $SPIN ]; then
