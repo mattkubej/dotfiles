341929838e2b35cc8896a5a7b79736c3eff707fe
Matt Kubej <mkubej@gmail.com>
2022-11-04 00:19:41 -0400

    add eslint_d

--- nvim/.config/nvim/lua/mk/plugins/configs/lsp/init.lua
+++ nvim/.config/nvim/lua/mk/plugins/configs/lsp/init.lua
@@ -6,7 +6,8 @@ null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettier.with({
       prefer_local = "node_modules/.bin",
-    })
+    }),
+    null_ls.builtins.diagnostics.eslint_d,
   }
 }
 
