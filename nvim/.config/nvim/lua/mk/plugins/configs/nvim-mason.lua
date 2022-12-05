4f3d89cdc04d6ef38385f301bce55eb2a22906c6
Matt Kubej <matt.kubej@shopify.com>
2022-12-05 15:43:31 -0500

    ruby_ls

--- nvim/.config/nvim/lua/mk/plugins/configs/nvim-mason.lua
+++ nvim/.config/nvim/lua/mk/plugins/configs/nvim-mason.lua
@@ -1,6 +1,6 @@
 require('mason').setup()
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "graphql", "ruby-ls", "rust_analyzer", "solargraph", "sorbet", "sumneko_lua", "tsserver",
+    ensure_installed = { "graphql", "ruby_ls", "rust_analyzer", "solargraph", "sorbet", "sumneko_lua", "tsserver",
         "tailwindcss" }
 })
