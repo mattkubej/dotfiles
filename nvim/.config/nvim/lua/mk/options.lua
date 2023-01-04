7c34eb21d5891a3ff651180af822bc20880d4c03
Matt Kubej <matt.kubej@shopify.com>
2023-01-04 09:58:07 -0500

    system clipboard in spin

--- nvim/.config/nvim/lua/mk/options.lua
+++ nvim/.config/nvim/lua/mk/options.lua
@@ -1,63 +1,78 @@
 local opt = vim.opt
 
-opt.guicursor      = ""
+opt.guicursor = ""
 
 opt.number         = true
 opt.relativenumber = true
 
-opt.hidden         = true
-opt.showmode       = false
-opt.updatetime     = 1000
-opt.hlsearch       = false
-opt.scrolloff      = 10
+opt.hidden     = true
+opt.showmode   = false
+opt.updatetime = 1000
+opt.hlsearch   = false
+opt.scrolloff  = 10
 
-opt.autoindent     = true
-opt.cindent        = true
-opt.smartindent    = true
+opt.autoindent  = true
+opt.cindent     = true
+opt.smartindent = true
 
-opt.expandtab      = true
-opt.shiftwidth     = 2
-opt.softtabstop    = 2
-opt.tabstop        = 2
+opt.expandtab   = true
+opt.shiftwidth  = 2
+opt.softtabstop = 2
+opt.tabstop     = 2
 
-opt.clipboard      = 'unnamedplus'
-opt.laststatus     = 2
+opt.laststatus = 2
 
-opt.undodir        = vim.fn.expand('$HOME/.vimdid//')
-opt.undofile       = true
-opt.swapfile       = false
-opt.inccommand     = 'split'
+opt.undodir    = vim.fn.expand('$HOME/.vimdid//')
+opt.undofile   = true
+opt.swapfile   = false
+opt.inccommand = 'split'
 
-opt.tm             = 500
+opt.tm = 500
+opt.updatetime = 50
 
-opt.incsearch      = true
-opt.smartcase      = true
+opt.incsearch = true
+opt.smartcase = true
 
-opt.belloff        = 'all'
+opt.belloff = 'all'
 
-opt.colorcolumn    = '80'
-opt.signcolumn     = 'yes:1'
-opt.termguicolors  = true
+opt.colorcolumn   = '80'
+opt.signcolumn    = 'yes:1'
+opt.termguicolors = true
 
-opt.wrap           = false
+opt.foldmethod = 'marker'
+opt.foldlevel  = 0
+opt.modelines  = 1
 
-opt.foldmethod     = 'marker'
-opt.foldlevel      = 0
-opt.modelines      = 1
+opt.joinspaces = false
+opt.wrap = false
 
-opt.joinspaces     = false
-
-opt.updatetime     = 50
-
-opt.formatoptions  = opt.formatoptions
-                     - '2'
-                     - 'a'
-                     - 'o'
-                     - 't'
-                     + 'c'
-                     + 'j'
-                     + 'n'
-                     + 'q'
-                     + 'r'
+opt.formatoptions = opt.formatoptions
+    - '2'
+    - 'a'
+    - 'o'
+    - 't'
+    + 'c'
+    + 'j'
+    + 'n'
+    + 'q'
+    + 'r'
 
 vim.g.etrw_dirhistmax = 0 -- do not save any netrw history or bookmarks
+
+opt.clipboard = 'unnamedplus'
+
+-- system clipboard within spin
+if os.getenv('SPIN') == '1' then
+  vim.g.clipboard = {
+    name = 'pbcopy',
+    copy = {
+      ['+'] = 'pbcopy',
+      ['*'] = 'pbcopy',
+    },
+    paste = {
+      ['+'] = 'pbpaste',
+      ['*'] = 'pbpaste',
+    },
+    cache_enabled = 1,
+  }
+end
