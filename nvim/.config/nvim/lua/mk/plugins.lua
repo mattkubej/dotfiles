e146fd398bc6cea6006483c5763cfa7495578257
Matt Kubej <matt.kubej@shopify.com>
2023-01-05 09:12:50 -0500

    remove editoconfig, built into neovim 0.9

--- nvim/.config/nvim/lua/mk/plugins.lua
+++ nvim/.config/nvim/lua/mk/plugins.lua
@@ -65,8 +65,6 @@ require('packer').startup(function(use)
     tag = 'nightly'
   }
 
-  use 'editorconfig/editorconfig-vim'
-
   use 'lewis6991/gitsigns.nvim'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
