8418d34b81d933c3f5ad50f499cfaeb873896aa2
Matt Kubej <matt.kubej@shopify.com>
2023-01-12 18:17:16 -0500

    speed up eslint with eslint_d

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -131,7 +131,7 @@ local null_ls = require('null-ls')
 null_ls.setup({
   debug = true,
   sources = {
-    null_ls.builtins.diagnostics.eslint.with({
+    null_ls.builtins.diagnostics.eslint_d.with({
       prefer_local = 'node_modules/.bin',
     }),
     null_ls.builtins.formatting.prettier.with({
