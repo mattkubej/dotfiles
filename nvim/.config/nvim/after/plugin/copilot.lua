361ea078198e1698b5851dc7af68dbe1118057e0
Matt Kubej <mkubej@gmail.com>
2023-04-10 00:14:37 -0400

    default copilot bindings, conflicted with escape

--- nvim/.config/nvim/after/plugin/copilot.lua
+++ nvim/.config/nvim/after/plugin/copilot.lua
@@ -1,6 +1,8 @@
 vim.keymap.set("i", "<C-j>", 'copilot#Accept("")', {expr = true, replace_keycodes = false})
 
-vim.keymap.set("i",  "<C-]>", "<Plug>(copilot-next)")
-vim.keymap.set("i",  "<C-[>", "<Plug>(copilot-previous)")
-vim.keymap.set("i",  "<C-s>", "<Plug>(copilot-suggest)")
-vim.keymap.set("i",  "<C-d>", "<Plug>(copilot-dismiss)")
+-- vim.keymap.set("i",  "<C-]>", "<Plug>(copilot-next)")
+-- vim.keymap.set("i",  "<C-[>", "<Plug>(copilot-previous)")
+-- vim.keymap.set("i",  "<C-s>", "<Plug>(copilot-suggest)")
+-- vim.keymap.set("i",  "<C-d>", "<Plug>(copilot-dismiss)")
+
+
