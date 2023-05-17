f78016c13657a71f336620dbfb99194a1235115d
Matt Kubej <matt.kubej@shopify.com>
2023-05-17 10:40:30 -0400

    spawns redundant eslint processes

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -143,9 +143,6 @@ local null_ls = require('null-ls')
 null_ls.setup({
   debug = true,
   sources = {
-    null_ls.builtins.diagnostics.eslint.with({
-      prefer_local = 'node_modules/.bin',
-    }),
     null_ls.builtins.formatting.prettier.with({
       prefer_local = 'node_modules/.bin',
     }),
