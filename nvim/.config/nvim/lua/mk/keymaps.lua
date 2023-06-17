38c22999649530470e19f85a9b6ff14194cd61c7
Matt Kubej <mkubej@gmail.com>
2023-06-17 13:45:22 -0400

    keymaps for saving and quiting

--- nvim/.config/nvim/lua/mk/keymaps.lua
+++ nvim/.config/nvim/lua/mk/keymaps.lua
@@ -33,3 +33,7 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>w", "<cmd>update<CR>")
+vim.keymap.set("n", "<leader>wq", "<cmd>wq<CR>")
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
