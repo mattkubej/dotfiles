70f9aff4c4a2811221905e7b1bd6559b4bab2107
Matt Kubej <mkubej@gmail.com>
2023-11-15 00:58:03 -0500

    show line numbers in outline

--- nvim/.config/nvim/after/plugin/outline.lua
+++ nvim/.config/nvim/after/plugin/outline.lua
@@ -1,6 +1,11 @@
 local ok, outline = pcall(require, 'outline')
 if not ok then return end
 
-outline.setup()
+outline.setup({
+  outline_window = {
+    show_numbers = true,
+    show_relative_numbers = true,
+  },
+})
 
 vim.keymap.set("n", "<leader>tt", '<cmd>Outline<CR>')
