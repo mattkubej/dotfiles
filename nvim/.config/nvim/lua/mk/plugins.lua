0cadda1a63ed08f91c5fe3281ded5dce0f7703e2
Matt Kubej <mkubej@gmail.com>
2023-12-05 23:49:21 -0500

    snips seem to be broken

--- nvim/.config/nvim/lua/mk/plugins.lua
+++ nvim/.config/nvim/lua/mk/plugins.lua
@@ -54,12 +54,12 @@ require("lazy").setup({
       'hrsh7th/nvim-cmp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
-      'saadparwaiz1/cmp_luasnip',
+      -- 'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lua',
 
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
+      -- 'L3MON4D3/LuaSnip',
+      -- 'rafamadriz/friendly-snippets',
 
       { 'j-hui/fidget.nvim', tag = 'legacy' },
       'folke/neodev.nvim',
