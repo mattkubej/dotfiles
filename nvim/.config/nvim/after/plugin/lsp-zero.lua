282cfa0a128cfdc65be6c5c03969aa26a30ae71d
Matt Kubej <matt.kubej@shopify.com>
2024-01-30 22:45:39 -0500

    move away from lsp-zero

---
+++ nvim/.config/nvim/after/plugin/lsp-zero.lua
@@ -0,0 +1,163 @@
+if true then
+  return
+end
+
+vim.g.lsp_zero_extend_lspconfig = 0
+
+local ok, lsp = pcall(require, 'lsp-zero')
+if not ok then return end
+
+lsp.set_preferences({
+  suggest_lsp_servers = true,
+  setup_servers_on_start = true,
+  set_lsp_keymaps = true,
+  configure_diagnostics = true,
+  cmp_capabilities = true,
+  manage_nvim_cmp = true,
+  call_servers = 'local',
+  sign_icons = {
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = ''
+  }
+})
+
+local servers = {
+  -- 'tsserver',
+  'lua_ls',
+  'rust_analyzer',
+  'graphql',
+  'html',
+  'pylsp',
+  'solargraph',
+  'sorbet',
+  'ruby_ls',
+  'rubocop',
+  'eslint',
+  'jsonls',
+  'stylelint_lsp',
+}
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = servers,
+  handlers = {
+    lsp.default_setup,
+    -- tsserver = function()
+    --   require('lspconfig').tsserver.setup({
+    --     cmd = { "typescript-language-server", "--stdio" },
+    --     initializationOptions = {
+    --       maxTsServerMemory = 8192,
+    --     },
+    --   })
+    -- end,
+  },
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('lua_ls', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+})
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<Tab>'] = nil,
+    ['<S-Tab>'] = nil,
+  }),
+  preselect = cmp.PreselectMode.None,
+  completion = {
+    completeopt = 'menu,menuone,noinsert,noselect',
+  },
+})
+
+lsp.set_preferences({
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
+})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
+
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-h>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  nmap('<leader>f', function()
+    vim.lsp.buf.format({ timeout_ms = 10000 })
+  end, 'Format current buffer with LSP')
+
+  if client.name == "typescript-tools" or client.name == "tsserver" then
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+  end
+end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+})
+
+require('fidget').setup()
+require('neodev').setup()
+
+local null_ls = require('null-ls')
+null_ls.setup({
+  debug = true,
+  sources = {
+    null_ls.builtins.formatting.prettier.with({
+      prefer_local = 'node_modules/.bin',
+    }),
+  }
+})
+require("mason-null-ls").setup({
+  ensure_installed = nil,
+  automatic_installation = true,
+  automatic_setup = false,
+})
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,64 +1,107 @@
-vim.g.lsp_zero_extend_lspconfig = 0
-
-local ok, lsp = pcall(require, 'lsp-zero')
+local ok, lsp = pcall(require, 'lspconfig')
 if not ok then return end
 
-lsp.set_preferences({
-  suggest_lsp_servers = true,
-  setup_servers_on_start = true,
-  set_lsp_keymaps = true,
-  configure_diagnostics = true,
-  cmp_capabilities = true,
-  manage_nvim_cmp = true,
-  call_servers = 'local',
-  sign_icons = {
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = ''
-  }
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local nmap = function(keys, func, desc)
+      if desc then
+        desc = 'LSP: ' .. desc
+      end
+
+      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc })
+    end
+
+    nmap('<space>e', vim.diagnostic.open_float, 'Open diagnostic float')
+    nmap('[d', vim.diagnostic.goto_prev, 'Go to previous diagnostic')
+    nmap(']d', vim.diagnostic.goto_next, 'Go to next diagnostic')
+
+    nmap('K', vim.lsp.buf.hover, 'Hover documentation')
+    nmap('gd', vim.lsp.buf.definition, 'Go to definition')
+    nmap('gD', vim.lsp.buf.declaration, 'Go to declaration')
+    nmap('gi', vim.lsp.buf.implementation, 'Go to implementation')
+    nmap('go', vim.lsp.buf.type_definition, 'Go to type definition')
+    nmap('gr', vim.lsp.buf.references, 'Go to references')
+    nmap('gs', vim.lsp.buf.signature_help, 'Signature documentation')
+
+    nmap('<leader>rn', vim.lsp.buf.rename, 'Rename')
+    nmap('<leader>ca', vim.lsp.buf.code_action, 'Code action')
+    nmap('<leader>f', function()
+      vim.lsp.buf.format({ timeout_ms = 10000 })
+    end, 'Format current buffer')
+  end
 })
 
-local servers = {
-  -- 'tsserver',
-  'lua_ls',
-  'rust_analyzer',
-  'graphql',
-  'html',
-  'pylsp',
-  'solargraph',
-  'sorbet',
-  'ruby_ls',
-  'rubocop',
-  'eslint',
-  'jsonls',
-  'stylelint_lsp',
-}
+vim.diagnostic.config({
+  virtual_text = true,
+})
+
+vim.diagnostic.config({
+  float = { border = 'rounded' },
+})
+
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = 'rounded' }
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  { border = 'rounded' }
+)
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local default_setup = function(server)
+  lsp[server].setup({
+    capabilities = lsp_capabilities,
+  })
+end
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = servers,
+  ensure_installed = {
+    'tsserver',
+    'lua_ls',
+    'rust_analyzer',
+    'graphql',
+    'html',
+    'pylsp',
+    'solargraph',
+    'sorbet',
+    'ruby_ls',
+    'rubocop',
+    'eslint',
+    'jsonls',
+    'stylelint_lsp',
+  },
   handlers = {
-    lsp.default_setup,
-    -- tsserver = function()
-    --   require('lspconfig').tsserver.setup({
-    --     cmd = { "typescript-language-server", "--stdio" },
-    --     initializationOptions = {
-    --       maxTsServerMemory = 8192,
-    --     },
-    --   })
-    -- end,
+    default_setup,
+    lua_ls = function()
+      lsp.lua_ls.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }
+            }
+          }
+        }
+      })
+    end,
   },
 })
-
--- Fix Undefined global 'vim'
-lsp.configure('lua_ls', {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
-      }
-    }
+require("mason-null-ls").setup({
+  ensure_installed = {},
+  automatic_installation = false,
+  handlers = {},
+})
+local null_ls = require('null-ls')
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.prettier.with({
+      prefer_local = 'node_modules/.bin',
+    }),
   }
 })
 
@@ -66,6 +109,13 @@ local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -81,79 +131,4 @@ cmp.setup({
   completion = {
     completeopt = 'menu,menuone,noinsert,noselect',
   },
-})
-
-lsp.set_preferences({
-  suggest_lsp_servers = false,
-  sign_icons = {
-    error = 'E',
-    warn = 'W',
-    hint = 'H',
-    info = 'I'
-  }
-})
-
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({ buffer = bufnr })
-
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-h>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  nmap('<leader>f', function()
-    vim.lsp.buf.format({ timeout_ms = 10000 })
-  end, 'Format current buffer with LSP')
-
-  if client.name == "typescript-tools" or client.name == "tsserver" then
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentFormattingRangeProvider = false
-  end
-end)
-
-lsp.setup()
-
-vim.diagnostic.config({
-  virtual_text = true,
-})
-
-require('fidget').setup()
-require('neodev').setup()
-
-local null_ls = require('null-ls')
-null_ls.setup({
-  debug = true,
-  sources = {
-    null_ls.builtins.formatting.prettier.with({
-      prefer_local = 'node_modules/.bin',
-    }),
-  }
-})
-require("mason-null-ls").setup({
-  ensure_installed = nil,
-  automatic_installation = true,
-  automatic_setup = false,
 })
