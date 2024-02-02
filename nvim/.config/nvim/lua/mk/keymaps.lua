b90c09c7ed022259929ed13d2db005781e6f2b1c
Matt Kubej <matt.kubej@shopify.com>
2024-02-01 22:41:32 -0500

    remap replace word

--- nvim/.config/nvim/lua/mk/keymaps.lua
+++ nvim/.config/nvim/lua/mk/keymaps.lua
@@ -25,7 +25,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>w", "<cmd>update<CR>")
 vim.keymap.set("n", "<leader>wq", "<cmd>wq<CR>")
