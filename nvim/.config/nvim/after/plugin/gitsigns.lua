cda87de0e3256a4c3c7ac94b50c4d4f627b002c4
Matt Kubej <matt.kubej@shopify.com>
2024-02-01 22:29:01 -0500

    git blame keymap

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -1,4 +1,16 @@
 local ok, gitsigns = pcall(require, 'gitsigns')
 if not ok then return end
 
-gitsigns.setup()
+gitsigns.setup({
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', '<leader>gb', function() gs.blame_line { full = true } end)
+  end
+})
