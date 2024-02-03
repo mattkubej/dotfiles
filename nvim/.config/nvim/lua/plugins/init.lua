f4375d9a35afc534641a02b5c58a03c0587034e9
Matt Kubej <mkubej@gmail.com>
2024-02-03 14:18:38 -0500

    move lualine

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -108,7 +108,24 @@ return {
     }
   },
   'tpope/vim-rhubarb',
-  'nvim-lualine/lualine.nvim',
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        theme = 'catppuccin',
+        icons_enabled = false,
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'hostname' },
+        lualine_c = { 'branch', { 'filename', path = 1 }, 'aerial' },
+        lualine_x = { 'diff', 'encoding', 'fileformat' }, -- filetype causes lag
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      extensions = { 'fugitive', 'fzf' }
+    }
+  },
   'tpope/vim-sleuth',
   {
     'junegunn/vim-easy-align',
