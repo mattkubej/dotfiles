287a2e76a343211b3b8a8e6cb6e5fc8c26a9d27b
Matt Kubej <mkubej@gmail.com>
2024-02-03 14:55:01 -0500

    move neotest

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -321,6 +321,20 @@ return {
       'antoinemadec/FixCursorHold.nvim',
       'haydenmeade/neotest-jest'
     },
+    opts = {
+      adapters = {
+        require('neotest-jest')({
+          jestCommand = "npm test -- --watch",
+        }),
+      },
+    },
+    keys = {
+      { "<leader>tw", "<cmd>lua require('neotest').run.run()<cr>" },
+      { "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand(\"%\"))<cr>" },
+      { "<leader>ts", "<cmd>lua require('neotest').run.stop()<cr>" },
+      { "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>" },
+      { "<leader>to", "<cmd>lua require('neotest').output_panel.toggle({ last_run = true })<cr>" },
+    },
   },
   {
     'echasnovski/mini.surround',
