d932788e869dbc05d5f52c071323040ed01ebd38
Matt Kubej <mkubej@gmail.com>
2024-02-03 14:26:34 -0500

    move nvim tree

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -82,7 +82,46 @@ return {
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
-    tag = 'nightly'
+    tag = 'nightly',
+    opts = {
+      auto_reload_on_write = false,
+      git = {
+        enable = false,
+        ignore = false,
+      },
+      view = {
+        relativenumber = true,
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * 0.5
+            local window_h = screen_h * 0.8
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * 0.5)
+        end,
+      },
+    },
+    keys = {
+      { "<C-n>",     vim.cmd.NvimTreeToggle },
+      { "<leader>n", vim.cmd.NvimTreeFindFile },
+    },
   },
   {
     'lewis6991/gitsigns.nvim',
