92192720196e8273a6a9590bc80098845161b6f2
Matt Kubej <mkubej@gmail.com>
2024-02-03 23:10:49 -0500

    lsp capabilities

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -90,7 +90,8 @@ return {
         { border = 'rounded' }
       )
 
-      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
       local default_setup = function(server)
         lsp[server].setup({
