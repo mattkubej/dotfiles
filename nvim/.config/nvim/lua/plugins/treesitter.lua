180902872189818c8794afa948f369ac9427edc2
Matt Kubej <mkubej@gmail.com>
2024-03-22 00:44:13 -0400

    change keymaps and add difftastic

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -69,20 +69,20 @@ return {
           enable = true,
           set_jumps = true,
           goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
+            [']v'] = '@function.outer',
+            [']c'] = '@class.outer',
           },
           goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
+            [']V'] = '@function.outer',
+            [']C'] = '@class.outer',
           },
           goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
+            ['[v'] = '@function.outer',
+            ['[c'] = '@class.outer',
           },
           goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
+            ['[V'] = '@function.outer',
+            ['[C'] = '@class.outer',
           },
         },
         swap = {
