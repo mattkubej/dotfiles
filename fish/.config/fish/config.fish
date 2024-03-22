ebbb4ac349aeede81592671bd814ae2a1b882dc3
Matt Kubej <matt.kubej@shopify.com>
2024-03-22 16:49:21 -0400

    switch back

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,11 +13,11 @@ function fish_user_key_bindings
 end
 
 # override ls with eza, if exists
-if command -v eza > /dev/null
-  abbr -a l 'eza'
-  abbr -a ls 'eza'
-  abbr -a ll 'eza -l'
-  abbr -a lll 'eza -la'
+if command -v exa > /dev/null
+  abbr -a l 'exa'
+  abbr -a ls 'exa'
+  abbr -a ll 'exa -l'
+  abbr -a lll 'exa -la'
 else
   abbr -a l 'ls'
   abbr -a ll 'ls -l'
