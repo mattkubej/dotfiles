6118d5f3a17e5b347aea78a535b26ce133d384cd
Matt Kubej <matt.kubej@shopify.com>
2024-04-17 11:24:55 -0400

    indent keymap

--- nvim/.config/nvim/lua/mk/keymaps.lua
+++ nvim/.config/nvim/lua/mk/keymaps.lua
@@ -12,6 +12,9 @@ vim.keymap.set("n", "<leader>-", "<cmd>vertical resize -5<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
