54d10c8d4b089514d31726de3104ae59628bb2d7
Matt Kubej <mkubej@gmail.com>
2024-04-21 23:28:07 -0400

    luasnip keymaps

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -80,6 +80,17 @@ return {
           }
         })
       })
+
+      local ls = require('luasnip')
+
+      vim.keymap.set({ "i", "s" }, "<C-n>", function() ls.jump(1) end, { silent = true })
+      vim.keymap.set({ "i", "s" }, "<C-p>", function() ls.jump(-1) end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<C-e>", function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end, { silent = true })
     end
   },
 }
