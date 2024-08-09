8360dcd36bbc6b140af99ace4107aecba0e2a75e
Matt Kubej <matt.kubej@shopify.com>
2024-08-09 11:01:29 -0400

    add llm support with anthropic

---
+++ nvim/.config/nvim/lua/plugins/llm.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    dir = '~/.config/nvim/lua/local-plugins/llm.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local system_prompt =
+        'You should replace the code that you are sent, only following the comments. Do not talk at all. Only output valid code. Do not provide any backticks that surround the code. Never ever output backticks like this ```. Any comment that is asking you for something should be removed after you satisfy them. Other comments should left alone. Do not output backticks'
+      local helpful_prompt = 'You are a helpful assistant. What I have sent are my notes so far. You are very curt, yet helpful.'
+      local llm = require('llm')
+
+      local function anthropic_help()
+        llm.invoke_llm_and_stream_into_editor({
+          url = 'LLM_PROXY',
+          model = 'anthropic:claude-3-5-sonnet',
+          api_key_name = 'ANTHROPIC_TOKEN',
+          system_prompt = helpful_prompt,
+          replace = false,
+        }, llm.make_spec_curl_args, llm.handle_spec_data)
+      end
+
+      local function anthropic_replace()
+        llm.invoke_llm_and_stream_into_editor({
+          url = 'LLM_PROXY',
+          model = 'anthropic:claude-3-5-sonnet',
+          api_key_name = 'ANTHROPIC_TOKEN',
+          system_prompt = system_prompt,
+          replace = true,
+        }, llm.make_spec_curl_args, llm.handle_spec_data)
+      end
+
+      vim.keymap.set({ 'n', 'v' }, '<leader>L', anthropic_help, { desc = 'llm anthropic_help' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>l', anthropic_replace, { desc = 'llm anthropic' })
+    end,
+  },
+}
