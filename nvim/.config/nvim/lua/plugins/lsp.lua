abc607076ff7d5447b876a881d4602bfeb4e8c5f
Matt Kubej <mkubej@gmail.com>
2026-02-07 15:16:29 -0500

    improvements

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -119,26 +119,11 @@ return {
             vim.lsp.inlay_hint.enable(false)
           end
 
-          -- Document highlight on cursor hold
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
         end
       })
 
       vim.diagnostic.config({
         virtual_text = true,
-      })
-
-      vim.diagnostic.config({
         float = { border = 'rounded' },
       })
 
